701a250577ee444d1fa4d9463b9c24b8de923d1b
dyo1319 <raad_asded@hotmail.com>
2025-08-05 01:12:01 +0300

    Added basic ActivitiesPage with empty list and add button (placeholder functionality)

--- lib/pages/ActivitiesPage.dart
+++ lib/pages/ActivitiesPage.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 
-class ActivitiesPage extends StatelessWidget {
+class ActivitiesPage extends StatefulWidget {
   const ActivitiesPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child:  Text(
-        "Activites Page",
-        style: TextStyle(fontSize: 24,
-        fontWeight: FontWeight.bold
-        ),
+  State<ActivitiesPage> createState() => _ActivitiesPageState();
+}
+
+class _ActivitiesPageState extends State<ActivitiesPage> {
+  List<String> activities = [];
+
+  void _addActivity(String newActivity) {
+    setState(() {
+      activities.add(newActivity);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: activities.isEmpty
+          ? const Center(child: Text('אין פעילויות עדיין'))
+          : ListView.builder(
+          itemCount: activities.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: const Icon(Icons.event_note),
+              title: Text(activities[index]),
+            );
+          },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addActivity('פעילות חדשה');
+        },
+        backgroundColor: Colors.teal,
+        child: const Icon(Icons.add),
+        tooltip: 'הוסף פעילות',
       ),
     );
   }
